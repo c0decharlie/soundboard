97bf4b2c322cf77c2a3db6adc74e7b4b20936582
Cezary Gradowski <gradowski.c@gmail.com>
2019-12-07 13:17:33 +0100

    Vue basic setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
     entry: './app/index.js',
 
@@ -7,6 +9,36 @@ module.exports = {
         path: path.resolve(__dirname, 'public', 'scripts'),
         filename: 'main.bundle.js'
     },
+
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+           test: /\.vue$/,
+           loader: 'vue-loader'
+         }
+        ]
+     },
+
+     plugins: [
+         new VueLoaderPlugin
+     ],
+
+     resolve: {
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+      },
 
     mode: 'development'
 }
