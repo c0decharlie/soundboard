bc2942ad8d15c72ee3f0f9bc3d3fa03cf18e2926
Cezary Gradowski <gradowski.c@gmail.com>
2019-12-29 11:43:15 +0100

    Active nav elements

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -3,7 +3,7 @@
     <nav>
       <div class="nav-wrapper">
         <a href="/" class="brand-logo">Soundboard</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <ul id="nav-mobile" class="right hide-on-med-and-down" ref="nav" @click="onNavClick">
           <li>
             <router-link to="/"> Mainboard</router-link>
           </li>
@@ -15,3 +15,36 @@
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      navList: null
+    }
+  },
+
+  methods: {
+    removeActiveClassesFromNav() {
+      this.navList.querySelectorAll('li').forEach(element => {
+        element.classList.remove('active');
+      });
+    },
+
+    addActiveClass(element) {
+      element.classList.add('active');
+    },
+
+    onNavClick(e) {
+      this.removeActiveClassesFromNav();
+      this.addActiveClass(e.target.parentNode);
+    }
+  },
+
+  mounted() {
+    this.navList = this.$refs.nav;
+    const activeNavElement = this.navList.querySelector('.active').parentNode;
+    this.addActiveClass(activeNavElement);
+  }
+}
+</script>
