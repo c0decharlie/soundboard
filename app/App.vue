f9cfafdf7ffca5fe671c33dc637071d50e776478
Cezary Gradowski <gradowski.c@gmail.com>
2020-01-05 13:50:06 +0100

    Add info message if theres no audio files

--- app/App.vue
+++ app/App.vue
@@ -1,47 +1,16 @@
 <template>
     <div>
-
         <Navigation />
-
-        <!-- <AudioFileUpload 
-            @uploadFile="onFileUpload"
-        />
-        <h2>Files:</h2>
-
-        
-
-        <hr>
-
-        <AudioPlayer
-            :audioFile="currentAudioFile"
-        /> -->
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-import FileService from './services/file.service';
-
-import AudioFileUpload from './components/AudioFileUpload';
-import AudioList from './components/AudioList';
-import AudioPlayer from './components/AudioPlayer';
 import Navigation from './components/Navigation';
 
 export default {
     components: {
-        AudioFileUpload,
-        AudioList,
-        AudioPlayer,
         Navigation
-    },
-
-    data() {
-        return {
-            fileService: new FileService(),
-            audioFiles: [],
-            currentAudioFile: null
-        }
-    },
-
+    }
 }
 </script>
--- app/views/Mainboard.vue
+++ app/views/Mainboard.vue
@@ -1,16 +1,25 @@
 <template>
     <div class="container">
-        <AudioList 
-            :audioFiles="audioFiles"
-            @audioListElementClick="onAudioListElementClick"
-            @audioListElementDelete="onAudioListElementDelete"
-        />
+        <template v-if="audioFiles.length !== 0">
+            <AudioList 
+                :audioFiles="audioFiles"
+                @audioListElementClick="onAudioListElementClick"
+                @audioListElementDelete="onAudioListElementDelete"
+            />
 
-        <AudioPlayer
-            ref="audioPlayer"
-            :audioFile="currentAudioFile"
-            autoPlay
-        />
+            <AudioPlayer
+                ref="audioPlayer"
+                :audioFile="currentAudioFile"
+                autoPlay
+            />
+        </template>
+
+        <template v-if="audioFiles.length === 0">
+            <div class="info-container">
+                <h3>There's no audio files to show.</h3>
+                <p>To add audio files go to management page.</p>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -44,7 +53,6 @@ export default {
         },
 
         onAudioListElementClick(audioFile) {
-            debugger;
             if (!this.currentAudioFile) {
                 this.currentAudioFile = audioFile;
                 return;
@@ -87,3 +95,21 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    .container {
+        height: 100%;
+    }
+
+    .info-container {
+        text-align: center;
+
+        h3 {
+            color: #404040;
+        }
+
+        p {
+            color: #676767;
+        }
+    }
+</style>
